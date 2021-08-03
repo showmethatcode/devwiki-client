311539e955628b5edfefe2920d62659430b289e0
Woosik Kim <iam.woosik.kim@gmail.com>
2021-08-03 22:04:18 +0900

    🎉 Term 타입 추가

--- src/screen/MainScreen/MainScreen.tsx
+++ src/screen/MainScreen/MainScreen.tsx
@@ -1,6 +1,13 @@
 import React, { FC } from 'react'
 
+type Term = {
+  id: number
+  title: string
+}
+
 const MainScreen: FC = () => {
+  const latestTerms: Term[] = []
+  const popularTerms: Term[] = []
   return (
     <>
       <div>MainScreen</div>
