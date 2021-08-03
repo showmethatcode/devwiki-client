b31d881c3c30f3317b93efadffb64398aab689ef
Woosik Kim <iam.woosik.kim@gmail.com>
2021-08-04 03:32:22 +0900

    🔥 불필요한 주석 제거

--- @types/@emotion/index.d.ts
+++ @types/@emotion/index.d.ts
@@ -1,11 +1,6 @@
 import { InterpolationWithTheme } from '@emotion/core'
-// import generateTheme from 'styles/theme'
-// import { Theme as MUITheme } from '@material-ui/core'
-
-// type Thema = ReturnType<typeof generateTheme>
 
 // https://github.com/emotion-js/emotion/issues/1197
-
 declare module 'react' {
   interface DOMAttributes<T> {
     css?: InterpolationWithTheme<Theme>
@@ -14,11 +9,6 @@ declare module 'react' {
 
 declare global {
   namespace JSX {
-    /**
-     * Do we need to modify `LibraryManagedAttributes` too,
-     * to make `className` props optional when `css` props is specified?
-     */
-
     interface IntrinsicAttributes {
       css?: InterpolationWithTheme<Theme>
     }
