977dd0134565107bfae524fccad0387e81a77b5a
박상민 <iamsangminpark@gmail.com>
2021-11-10 18:01:18 +0900

    Detail 페이지 컴포넌트 추가

---
+++ src/components/Terms/Detail/DetailTerm.tsx
@@ -0,0 +1,30 @@
+import React, { FC, useEffect, useState } from 'react'
+import axios from 'axios'
+
+const DetailTerm: React.FC = () => {
+  const [users, setUsers] = useState(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        setUsers(response.data)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    fetchData()
+  }, [])
+
+  if (!users) return null
+  return (
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>
+          {user.username} ({user.name})
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default DetailTerm
