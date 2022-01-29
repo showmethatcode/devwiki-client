da61c880a9e9a440de9650eed87cdb2a29ef60a2
steadily-worked <iamsangminpark@gmail.com>
2022-01-29 23:08:43 +0900

    🔨 환경변수 설정

--- src/pages/terms/[id].tsx
+++ src/pages/terms/[id].tsx
@@ -1,38 +1,50 @@
 import React from 'react'
 import useSWR from 'swr'
-import { css } from '@emotion/react'
+import {
+  wikiScreenStyle,
+  inputTitleStyle,
+  titleBlockStyle,
+  editButtonStyle,
+  listTermsStyle,
+  relatedTermsStyle,
+} from 'components/Terms/Detail/styles'
 import fetcher from 'utils/fetcher'
+import { server } from 'config'
 
-interface User {
+interface Term {
   id: number
   name: string
-  email: string
-  username: string
+  description: string
+  createdAt: string
+  updatedAt: string
+  related: { id: number; text: string }[]
 }
 
-const wikiScreenStyle = css`
-  width: 951px;
-  margin: 0 auto;
-`
+interface RelatedTerm {
+  id: number
+  text: string
+}
 
 const DetailTerm = ({ post, id }) => {
-  const { data, error } = useSWR<User[]>(
-    `https://jsonplaceholder.typicode.com/users/${id}`,
-    fetcher,
-    {
-      data: post,
-    },
-  )
+  const { data, error } = useSWR<Term>(`terms/${id}`, fetcher, { data: post })
 
   if (!data || error) return <div>No Data!</div>
 
   return (
     <div css={wikiScreenStyle}>
-      <ul key={data.id}>
-        <h1>
-          name: {data.name} (username: {data.username})
-        </h1>
-        <h2>email: {data.email}</h2>
+      <div css={titleBlockStyle}>
+        <h1 css={inputTitleStyle}>{data.name}</h1>
+        <button css={editButtonStyle}>
+          <span>📝 내용 개선하기</span>
+        </button>
+      </div>
+      <div>{data.description}</div>
+      <ul css={listTermsStyle}>
+        {data.related.map((relatedTerm: RelatedTerm) => (
+          <li css={relatedTermsStyle} key={relatedTerm.id}>
+            {relatedTerm.text}
+          </li>
+        ))}
       </ul>
     </div>
   )
@@ -41,7 +53,7 @@ const DetailTerm = ({ post, id }) => {
 export default DetailTerm
 
 export async function getStaticPaths() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users/')
+  const res = await fetch(`${server}/terms`)
   const posts = await res.json()
   const paths = posts.map((post) => ({
     params: { id: post.id.toString() },
@@ -50,9 +62,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${params.id}`,
-  )
+  const res = await fetch(`${server}/terms/${params.id}`)
   const post = await res.json()
 
   return { props: { post, id: params.id } }
--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
-const fetcher = (url: string) =>
-  axios
-    .get(url, {
+const fetcher = (url: string) => {
+  return axios
+    .get(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, {
       withCredentials: true,
     })
     .then((response) => response.data)
+}
 
 export default fetcher
