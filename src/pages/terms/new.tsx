8fa7abaa8029e3bae2280aa6a8850620bc34bf38
steadily-worked <iamsangminpark@gmail.com>
2022-01-31 11:21:48 +0900

    🚜 폴더명 변경

---
+++ src/pages/terms/new.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TermScreen from 'screen/TermScreen'
+
+const New = () => {
+  return (
+    <div>
+      <TermScreen />
+    </div>
+  )
+}
+
+export default New
+
+// localhost:3000/terms/new
