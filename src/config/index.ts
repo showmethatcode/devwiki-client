4c2b370688648777bfa4a83a4ebd6e214a9545fc
steadily-worked <iamsangminpark@gmail.com>
2022-02-02 21:04:41 +0900

    🚜  URL 폴더 구조 변경

--- src/config/index.ts
+++
@@ -1,5 +0,0 @@
-const dev = process.env.NODE_ENV !== 'production'
-
-export const server = dev
-  ? 'http://localhost:8000'
-  : `${process.env.NEXT_PUBLIC_API_URL}`
--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,1 +1,11 @@
 export const GITHUB_ORGANIZATION_URL = 'https://github.com/showmethatcode'
+
+const dev = process.env.NODE_ENV !== 'production'
+
+export const client = dev
+  ? 'http://localhost:3000'
+  : 'http://devwiki-client.vercel.app'
+
+export const server = dev
+  ? 'http://localhost:8000'
+  : `${process.env.NEXT_PUBLIC_API_URL}`
