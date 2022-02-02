4e684a06b922acfe587636853a6729cb9a64a377
steadily-worked <iamsangminpark@gmail.com>
2022-02-02 21:09:30 +0900

    💄 스타일 추가

---
+++ src/components/Terms/Detail/styles.tsx
@@ -0,0 +1,47 @@
+import { css } from '@emotion/react'
+
+export const wikiScreenStyle = css`
+  width: 951px;
+  margin: 0 auto;
+`
+
+export const inputTitleStyle = css`
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  margin-top: 60px;
+`
+export const titleBlockStyle = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const editButtonStyle = css`
+  margin-top: 52px;
+  background-color: #f3f3f3;
+  border: none;
+  font-weight: bold;
+  font-size: 18px;
+  color: #535353;
+  border-radius: 10px;
+  padding: 18px 33px;
+`
+
+export const listTermsStyle = css`
+  width: 100%;
+  padding: 0;
+`
+
+export const relatedTermsStyle = css`
+  padding: 8px 12px;
+  float: left;
+  border-radius: 18px;
+  font-weight: 500;
+  min-width: fit-content;
+  margin: 5px 5px 0px 0px;
+  color: white;
+  background-color: #7d68ff;
+  list-style: none;
+`
