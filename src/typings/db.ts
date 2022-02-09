ec7034dd4109fa50157df4429079ba51f8b56875
steadily-worked <iamsangminpark@gmail.com>
2022-02-09 19:07:13 +0900

    🎨 인터페이스 분리

--- src/typings/db.ts
+++ src/typings/db.ts
@@ -2,8 +2,35 @@ export interface Terms {
   name: string
 }
 
+export interface Term {
+  id: number
+  name: string
+  description: string
+  createdAt: string
+  updatedAt: string
+  related: { id: number; name: string }[]
+}
+
 export interface Props {
   relatedTerms: Terms[]
   setRelatedTerms?: (content: Terms[]) => void
   isRelatedTermsEmpty?: boolean
+}
+
+export interface DetailTermProps {
+  id: number
+}
+
+export interface RelatedTerm {
+  id: number
+  text: string
+}
+
+export interface Response {
+  id: number
+  name: string
+  description: string
+  createdAt: string
+  updatedAt: string
+  termRelatedNames: { id: number; name: string }[]
 }
