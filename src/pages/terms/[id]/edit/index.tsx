1fdef65b3c923f8e54b92cd3a26996a4ec991410
steadily-worked <iamsangminpark@gmail.com>
2022-02-22 23:38:22 +0900

    ✨ 용어 수정 페이지

---
+++ src/pages/terms/[id]/edit/index.tsx
@@ -0,0 +1,175 @@
+import React, { useState, Fragment } from 'react'
+import { css } from '@emotion/react'
+import { useRouter } from 'next/dist/client/router'
+import getDataFromLocalStorage from 'utils/getDataFromLocalStorage'
+import RelatedTerms from 'components/Terms/New/RelatedTerms'
+import Markdown from 'components/Terms/Markdown/MarkdownEditor'
+import '@toast-ui/editor/dist/toastui-editor.css'
+import axios from 'axios'
+import { server, client } from 'constants/common'
+import { Terms } from 'typings/db'
+import {
+  submitButtonStyle,
+  submitButtonNotReadyStyle,
+  inputTitleInEditModeStyle,
+} from 'components/Terms/New/styles'
+
+const termScreenStyle = css`
+  width: 718px;
+  margin: 0 auto;
+`
+
+const TermScreen = () => {
+  const router = useRouter()
+  const data = router.query
+
+  const termId = data.id
+  const initialName: string | string[] = data.name
+  const initialDescription: string | string[] = data.description
+  const initialTermsRelated = (data.termsRelated as unknown) as Terms[]
+
+  const token = getDataFromLocalStorage('token')
+
+  const [relatedTerms, setRelatedTerms] = useState<Terms[]>([])
+
+  const [markdownContent, setMarkdownContent] = useState([])
+  const isMarkdownContentEmpty = markdownContent.length === 0
+
+  const onClickSubmit = () => {
+    axios
+      .post(
+        `${server}/terms/`,
+        {
+          name: initialName,
+          description: markdownContent,
+          termsRelated: relatedTerms,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+      .then((res) => {
+        console.log(res.data)
+        alert('수정되었습니다')
+        router.push(`${client}/terms/${termId}`)
+      })
+      .catch((err) => console.error(err))
+  }
+
+  return (
+    <Fragment>
+      <div id="container" css={termScreenStyle}>
+        <h2>추가할 용어</h2>
+        <input
+          readOnly
+          css={inputTitleInEditModeStyle}
+          value={initialName}
+          type="text"
+          id="title_input"
+        />
+        <Markdown
+          initialValue={initialDescription}
+          setMarkdownContent={setMarkdownContent}
+          isMarkdownContentEmpty={isMarkdownContentEmpty}
+        />
+        <RelatedTerms
+          setRelatedTerms={setRelatedTerms}
+          relatedTerms={initialTermsRelated}
+        />
+        <button
+          css={
+            !isMarkdownContentEmpty
+              ? submitButtonStyle
+              : submitButtonNotReadyStyle
+          }
+          disabled={!isMarkdownContentEmpty ? false : true}
+          id="submit"
+          onClick={onClickSubmit}
+        >
+          <span>저장하기</span>
+        </button>
+      </div>
+    </Fragment>
+  )
+}
+
+export default TermScreen
+//   const token = getDataFromLocalStorage('token')
+//   const [titleInput, setTitleInput] = useState('')
+//   const isTitleInputEmpty = titleInput.length === 0
+//   const onChangeInputTitle = useCallback(
+//     (e: React.ChangeEvent<HTMLInputElement>) => {
+//       setTitleInput(e.target.value)
+//     },
+//     [titleInput],
+//   )
+
+//   const [relatedTerms, setRelatedTerms] = useState<Terms[]>([])
+//   const isRelatedTermsEmpty = relatedTerms.length === 0
+
+//   const [markdownContent, setMarkdownContent] = useState([])
+//   const isMarkdownContentEmpty = markdownContent.length === 0
+
+//   const onClickSubmit = () => {
+//     axios
+//       .post(
+//         `${server}/terms/`,
+//         {
+//           name: titleInput,
+//           description: markdownContent,
+//           termsRelated: relatedTerms,
+//         },
+//         {
+//           headers: {
+//             'Content-Type': 'application/json',
+//             Authorization: `Bearer ${token}`,
+//           },
+//         },
+//       )
+//       .then((res) => {
+//         console.log(res.data)
+//         alert('등록되었습니다')
+//         router.push(`${client}`)
+//       })
+//       .catch((err) => console.error(err))
+//   }
+
+//   return (
+//     <>
+//       <div id="container" css={termScreenStyle}>
+//         <h2>추가할 용어</h2>
+//         <input
+//           css={inputTitleStyle}
+//           value={titleInput}
+//           onChange={onChangeInputTitle}
+//           type="text"
+//           id="title_input"
+//         />
+//         <Markdown
+//           setMarkdownContent={setMarkdownContent}
+//           isMarkdownContentEmpty={isMarkdownContentEmpty}
+//         />
+//         <RelatedTerms
+//           setRelatedTerms={setRelatedTerms}
+//           relatedTerms={relatedTerms}
+//           isRelatedTermsEmpty={isRelatedTermsEmpty}
+//         />
+//         <button
+//           css={
+//             !isTitleInputEmpty ? submitButtonStyle : submitButtonNotReadyStyle
+//           }
+//           disabled={!isTitleInputEmpty ? false : true}
+//           id="submit"
+//           onClick={onClickSubmit}
+//         >
+//           <span>저장하기</span>
+//         </button>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default TermScreen
