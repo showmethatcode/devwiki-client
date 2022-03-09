b4fedea9f2c853325733a19e5e6039a248f14d90
steadily-worked <iamsangminpark@gmail.com>
2022-03-09 16:41:12 +0900

    ✨ 용어 수정 기능 구현

--- src/pages/terms/[id]/edit/index.tsx
+++ src/pages/terms/[id]/edit/index.tsx
@@ -7,42 +7,38 @@ import Markdown from 'components/Terms/Markdown/MarkdownEditor'
 import '@toast-ui/editor/dist/toastui-editor.css'
 import axios from 'axios'
 import { server, client } from 'constants/common'
-import { Terms } from 'typings/db'
+import { Terms, DetailTermProps, RelatedTerm } from 'typings/db'
 import {
   submitButtonStyle,
   submitButtonNotReadyStyle,
   inputTitleInEditModeStyle,
 } from 'components/Terms/New/styles'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { QueryClient, dehydrate, useQuery } from 'react-query'
+import { getTerms } from 'apis/getTerms'
 
 const termScreenStyle = css`
   width: 718px;
   margin: 0 auto;
 `
 
-const TermScreen = () => {
+const EditScreen = ({ id }: DetailTermProps) => {
   const router = useRouter()
-  const data = router.query
-
-  const termId = data.id
-  const initialName: string | string[] = data.name
-  const initialDescription: string | string[] = data.description
-  const initialTermsRelated = (data.termsRelated as unknown) as Terms[]
+  const { data, isLoading } = useQuery(['terms', id], () =>
+    getTerms(id),
+  )
 
   const token = getDataFromLocalStorage('token')
-
   const [relatedTerms, setRelatedTerms] = useState<Terms[]>([])
-
   const [markdownContent, setMarkdownContent] = useState([])
   const isMarkdownContentEmpty = markdownContent.length === 0
-
   const onClickSubmit = () => {
     axios
-      .post(
-        `${server}/terms/`,
+      .put(
+        `${server}/terms/${id}`,
         {
-          name: initialName,
           description: markdownContent,
-          termsRelated: relatedTerms,
+          termRelatedNames: relatedTerms,
         },
         {
           headers: {
@@ -54,10 +50,20 @@ const TermScreen = () => {
       .then((res) => {
         console.log(res.data)
         alert('수정되었습니다')
-        router.push(`${client}/terms/${termId}`)
+        router.push(`${client}/terms/${id}`)
       })
-      .catch((err) => console.error(err))
+      .catch((err) => {
+        console.error(err)
+        console.log('실패')
+      })
   }
+
+  if (isLoading || !data) {
+    return <div>No Data!</div>
+  }
+  const termRelatedNames = data.data.term.termsRelated.map(
+    (term: RelatedTerm) => term.name,
+  )
 
   return (
     <Fragment>
@@ -66,18 +72,18 @@ const TermScreen = () => {
         <input
           readOnly
           css={inputTitleInEditModeStyle}
-          value={initialName}
+          value={data.data.term.name}
           type="text"
           id="title_input"
         />
         <Markdown
-          initialValue={initialDescription}
+          initialValue={data.data.term.description || ''}
           setMarkdownContent={setMarkdownContent}
           isMarkdownContentEmpty={isMarkdownContentEmpty}
         />
         <RelatedTerms
           setRelatedTerms={setRelatedTerms}
-          relatedTerms={initialTermsRelated}
+          relatedTerms={termRelatedNames}
         />
         <button
           css={
@@ -96,4 +102,34 @@ const TermScreen = () => {
   )
 }
 
-export default TermScreen
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  try {
+    if (context?.params?.id) {
+      const id = Number(context.params.id)
+      const queryClient = new QueryClient()
+
+      await queryClient.prefetchQuery('terms', () => getTerms(id), {
+        staleTime: 1000,
+      })
+
+      return {
+        props: {
+          dehydratedState: dehydrate(queryClient).toString(),
+          id: context.params.id,
+        },
+      }
+    }
+  } catch (error) {
+    console.error('Error', error)
+  }
+
+  return {
+    props: {
+      id: context.params.id,
+    },
+  }
+}
+
+export default EditScreen
